6eab886b0673690007ab4941e0ae225e5911e955
Landeiras <landeirasluis@gmail.com>
2024-06-11 19:07:45 +0100

    In Process

--- XGBoost.py
+++ XGBoost.py
@@ -3,39 +3,29 @@ import xgboost as xgb
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix
 import matplotlib.pyplot as plt
-import seaborn as sns
 from xgboost import plot_importance
 import warnings,AuxFun
+import seaborn as sns
 
-def XGBoostTeste():
+def XGBoostTrain(data):
     warnings.filterwarnings('ignore', category=FutureWarning)
     
     data = pd.read_csv("DataV2.csv")
-
-    X = data.drop(columns=['Text','Classification'])
+    
+    X = data.drop(columns=['Text', 'Classification'])
     y = data['Classification']
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
 
-    model = xgb.XGBClassifier(label_encoder=False, eval_metric='logloss')
+    model = xgb.XGBClassifier(use_label_encoder=False, eval_metric='logloss')
     model.fit(X_train, y_train)
 
     y_pred = model.predict(X_test)
-
     accuracy = accuracy_score(y_test, y_pred)
-
-    #plot_importance(model, importance_type='gain', max_num_features=14)
-    #plt.show()
-    #
-    #cm = confusion_matrix(y_test, y_pred)
-    #plt.figure(figsize=(10, 6))
-    #sns.heatmap(cm, annot=True, fmt='d', cmap='Blues')
-    #plt.xlabel('Predicted Label')
-    #plt.ylabel('True Label')
-    #plt.title('Confusion Matrix')
-    #plt.show()
     
-    return round(accuracy*100,2)
+    if(round(accuracy * 100,2) > 90): model.save_model('XGBModelS.txt')
+
+    print(round(accuracy * 100,2))
 
 def preprocess_text(text):
     Amostra = AuxFun.Amostras(text)
@@ -49,50 +39,54 @@ def preprocess_text(text):
         'LexicalDiversity': Resultado['LexicalDiversity'][0],
         'Reading': Resultado['Reading'][0],
         'SentenceLength': Resultado['SentenceLength'][0],
-        #'SentimentNeg': Resultado['Sentiment'][0],
-        #'SentimentNeu': Resultado['Sentiment'][1],
-        #'SentimentPos': Resultado['Sentiment'][2],
+        'SentimentNeg': Resultado['Sentiment'][0],
+        'SentimentNeu': Resultado['Sentiment'][1],
+        'SentimentPos': Resultado['Sentiment'][2],
         'Smog': Resultado['Smog'][0],
         'Tree': Resultado['Tree'][0],
         'WordLength': Resultado['WordLength'][0],
     }])
 
-def XGBoost(data, new_text=None):
-    warnings.filterwarnings('ignore', category=FutureWarning)
+def XGBoostPredict(Texto):
+    model = xgb.XGBClassifier()
+    model.load_model('XGBModelS.txt')
     
-    X = data.drop(columns=['Text', 'Classification','SentimentNeg','SentimentNeu','SentimentPos'])
-    y = data['Classification']
-
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
-
-    model = xgb.XGBClassifier(use_label_encoder=False, eval_metric='logloss')
-    model.fit(X_train, y_train)
-
-    y_pred = model.predict(X_test)
-    accuracy = accuracy_score(y_test, y_pred)
+    #data = pd.read_csv("DataV2.csv")
+    #feature_names = data.drop(columns=['Text', 'Classification','SentimentNeg','SentimentNeu','SentimentPos']).columns
+    #plot_importance(model, importance_type='gain', max_num_features=10)
+    #plt.gca().set_yticklabels(feature_names) 
+    #plt.show()
     
-    p = 0
+    #cm = confusion_matrix(y_test, y_pred)
+    #plt.figure(figsize=(10, 6))
+    #sns.heatmap(cm, annot=True, fmt='d', cmap='Blues')
+    #plt.xlabel('Predicted Label')
+    #plt.ylabel('True Label')
+    #plt.title('Confusion Matrix')
+    #plt.show()
     
-    if new_text:
-        new_text_processed = preprocess_text(new_text)
-        prediction = model.predict(new_text_processed)
-        p = prediction[0]
-
-    return round(accuracy * 100, 2), p
-
-#def main():
-#    data = pd.read_csv("DataV2.csv")
-#    Soma = 0
-#    for i in range(100):
-#        new_text = AuxFun.File("Texto.txt")
-#        classification = XGBoost(data, new_text)
-#        print(f"Iteration {i+1} Classification for new text: {classification[1]}")
-#        print(f"Iteration {i+1} Accuracy for new text: {classification[0]}")
-#        Soma += classification[0]
-#
-#    print("Average Accuracy:", Soma / 100)
+    TextoP = preprocess_text(Texto)
     
-
+    sentiment_dict = {'SentimentNeg': TextoP['SentimentNeg'].iloc[0],'SentimentNeu': TextoP['SentimentNeu'].iloc[0],'SentimentPos': TextoP['SentimentPos'].iloc[0]}
     
-    
+    SentName = max(sentiment_dict, key=sentiment_dict.get)
 
+    if SentName == 'SentimentNeu': SentimentName = "Neutro"
+    if SentName == 'SentimentPos': SentimentName = "Positivo"
+    if SentName == 'SentimentNeg': SentimentName = "Negativo"
+    
+    SentimentValue = sentiment_dict[SentName]
+    
+    prediction = model.predict(TextoP)
+    
+    return prediction[0], SentimentName, SentimentValue
+
+#Accuracy 95.83
+#Accuracy Sent 91.67
+
+file = AuxFun.File("Texto3_2.txt")
+for _ in range(100):
+   print(XGBoostPredict(file))
+
+#for _ in range(10):
+#    XGBoostTrain("DataV2.csv")
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request
 import XGBoost
-import pandas as pd
 
 app = Flask(__name__)
 
@@ -8,12 +7,10 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
 
-@app.route('/sub', methods=['POST'])
+@app.route('/', methods=['POST'])
 def sub():
     result = request.form
     Texto = result.get('text')
-    data = pd.read_csv("DataV2.csv")
     
-    Resultado = XGBoost.XGBoost(data,Texto)
-    
-    return render_template('sub.html', Result=Resultado)
+    Resultado = XGBoost.XGBoostPredict(Texto)
+    return render_template('index.html', Result=Resultado)
